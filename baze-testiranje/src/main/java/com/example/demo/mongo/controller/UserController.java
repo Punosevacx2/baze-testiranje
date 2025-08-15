cf92dff28075f9cdfc80837130770f14e9e4ec96
Milos Punosevac <milosiveljko.punosevac@gmail.com>
2025-08-15 19:16:26 +0200

    poruka

--- baze-testiranje/src/main/java/com/example/demo/mongo/controller/UserController.java
+++ baze-testiranje/src/main/java/com/example/demo/mongo/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,6 +35,7 @@ public class UserController {
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public List<User> getAll() {
         return userService.getAllUsers();
     }
--- baze-testiranje/src/main/java/com/example/demo/mongo/entities/Role.java
+++ baze-testiranje/src/main/java/com/example/demo/mongo/entities/Role.java
@@ -17,7 +17,7 @@ public class Role {
         return name;
     }
 
-    public void setName(String name) {
+    public void	 setName(String name) {
         this.name = name;
     }
 
